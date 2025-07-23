fd0b717c6aad5fa6857e9460fb2bf6720c48c6b0
Angel Perez <angeld05@live.com>
2025-07-23 17:47:19 -0400

    rename install_nodejs.sh to install.sh and add git installation and repository cloning

---
+++ src/script/install.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Install git
+yum install git -y
+
+# Clone the repository
+https://github.com/Khahory/yt-ninjas.git
+
+# Install Node.js using NVM (Node Version Manager)
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
+source ~/.bashrc
+nvm install --lts
+node -v
+npm -v
+
+touch ~/creado-automaticamente.txt
--- src/script/install_nodejs.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
-source ~/.bashrc
-nvm install --lts
-node -v
-npm -v
