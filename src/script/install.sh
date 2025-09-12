3b3a641a2263ffb1561d2a325d645e04b892d08d
Angel Perez <angeld05@live.com>
2025-09-11 20:19:37 -0400

    update CI/CD configuration to enhance deployment process and security scanning

--- src/script/install.sh
+++ src/script/install.sh
@@ -10,18 +10,18 @@ dnf install -y git
 git clone https://github.com/Khahory/yt-ninjas.git /home/ec2-user/yt-ninjas
 
 # Instalar Node.js 18 desde Nodesource (compatible y LTS)
-curl -fsSL https://rpm.nodesource.com/setup_22.x | bash -
+curl -fsSL https://rpm.nodesource.com/setup_24.x | bash -
 dnf install -y nodejs
 
 # Verificar instalación
 node -v
 npm -v
 
-# Cambiar propietario del repo clonado (muy importante en user-data)
-chown -R ec2-user:ec2-user /home/ec2-user/yt-ninjas
-
 # Instalar dependencias del proyecto
 npm install --prefix /home/ec2-user/yt-ninjas
 
 # Crear un archivo de prueba
 touch /home/ec2-user/creado-automaticamente.txt
+
+# Cambiar propietario del repo clonado (muy importante en user-data)
+chown -R ec2-user:ec2-user /home/ec2-user
